a9b8dd9c5765d50ed057fda6be8b8e88e39f64a2
liadona <sabat.adons@gmail.com>
2020-09-12 12:48:39 +0700

    cm 2

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
 const Register = () => {
     const [ formData, setFormData ] = useState({
         name: '',
